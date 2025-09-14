cb079ef2238f054feac4b4e09bfcd6f0a98234d8
Karume-lab <danielkarume@outlook.com>
2025-09-14 12:20:43 +0300

    feat: :sparkles: add footer

---
+++ src/components/core/Footer.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+const Footer = () => {
+  return (
+    <footer className="text-center mx-auto max-w-xl my-4">
+      <Card className="bg-primary text-primary-foreground">
+        <CardContent>
+          Designed and Made with <span className="inline-block animate-bounce">🗿</span>, by
+          yours truly (me).
+        </CardContent>
+      </Card>
+    </footer>
+  );
+};
+
+export default Footer;
