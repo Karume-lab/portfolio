a90571f47532f4d857f50095b8ca52dfa2dfccfe
Karume-lab <danielkarume@outlook.com>
2025-11-10 07:23:04 +0300

    fix: :lipstick: improve layout

--- src/components/home/RecommendationLetters.tsx
+++ src/components/home/RecommendationLetters.tsx
@@ -1,17 +1,14 @@
+import Link from "next/link";
 import SectionHeader from "@/components/core/SectionHeader";
 import { Badge } from "@/components/ui/badge";
 import { recommendationLetters } from "@/data/recommendation-letters";
-import Link from "next/link";
 
 const RecommendationLetters = () => {
   return (
-    <section>
-      <SectionHeader
-        title="WHERE ARE THE RECOMMENDATION LETTERS?"
-        className="text-center"
-      />
+    <section className="w-7xl mx-auto px-6 sm:px-8 lg:px-12">
+      <SectionHeader title="WHERE ARE THE RECOMMENDATION LETTERS?" />
 
-      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 flex">
+      <div className="flex">
         {recommendationLetters.map((letter) => (
           <Link
             key={letter.id}
