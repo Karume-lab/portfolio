9aa77915ee080813f84d6cb4c40a8bc13f42158f
Karume-lab <danielkarume@outlook.com>
2025-11-10 07:12:36 +0300

    fix: :adhesive_bandage: make the nav permanently visible

--- src/components/core/NavigationPill.tsx
+++ src/components/core/NavigationPill.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import ViewResumeButton from "@/components/core/ViewResumeButton";
+import { ViewResumeButton } from "@/components";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -17,32 +14,11 @@ import {
 import { NAVIGATION_ITEMS } from "@/data";
 
 const NavigationPill = () => {
-  const [isVisible, setIsVisible] = useState(true);
-  let scrollTimeout: NodeJS.Timeout;
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsVisible(false);
-
-      clearTimeout(scrollTimeout);
-
-      scrollTimeout = setTimeout(() => {
-        setIsVisible(true);
-      }, 200);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      clearTimeout(scrollTimeout);
-    };
-  });
-
   return (
     <NavigationMenu
-      className={`fixed top-4 left-1/2 -translate-x-1/2 z-50 bg-primary rounded-md p-2 shadow-lg transition-opacity duration-300 ${
-        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
+      className={
+        "fixed top-4 left-1/2 -translate-x-1/2 z-50 bg-primary rounded-md p-2 shadow-lg transition-opacity duration-300"
+      }
     >
       <NavigationMenuList>
         {NAVIGATION_ITEMS.map(({ href, label, icon: Icon }) => (
@@ -52,9 +28,11 @@ const NavigationPill = () => {
                 <NavigationMenuLink asChild>
                   <Link
                     href={href}
-                    className="flex items-center gap-2 text-primary-foreground"
+                    className={
+                      "flex items-center gap-2 text-primary-foreground transition-colors"
+                    }
                   >
-                    <Icon className="w-5 h-5" />
+                    <Icon className="size-5" />
                     <TooltipContent side="bottom">
                       <h2>{label}</h2>
                     </TooltipContent>
@@ -64,6 +42,7 @@ const NavigationPill = () => {
             </TooltipTrigger>
           </Tooltip>
         ))}
+
         <div className="h-8 rounded-full w-0.5 bg-primary-foreground" />
         <ViewResumeButton />
       </NavigationMenuList>
