d426d2aec4cc3ee269d4ab5a5edde6da0405c8b1
Karume-lab <danielkarume@outlook.com>
2025-11-15 10:29:17 +0300

    fix: :adhesive_bandage: change from <Badge /> to <Button />

--- src/components/home/RecommendationLetters.tsx
+++ src/components/home/RecommendationLetters.tsx
@@ -1,7 +1,7 @@
 import { ExternalLink } from "lucide-react";
 import Link from "next/link";
 import SectionHeader from "@/components/core/SectionHeader";
-import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { recommendationLetters } from "@/data/recommendation-letters";
 
 const RecommendationLetters = () => {
@@ -11,23 +11,21 @@ const RecommendationLetters = () => {
       className="max-w-7xl w-full mx-auto px-6 sm:px-8 lg:px-12"
     >
       <SectionHeader title="WHERE ARE THE RECOMMENDATION LETTERS?" />
-
       <div className="flex flex-wrap justify-start gap-4 mt-6">
         {recommendationLetters.map((letter) => (
-          <Link
-            key={letter.id}
-            href={letter.href}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Badge
-              className="p-4 hover:bg-primary hover:text-primary-foreground transition-colors duration-300 flex items-center gap-2"
-              variant={"outline"}
-            >
-              {letter.company}
-              <ExternalLink className="size-4" />
-            </Badge>
-          </Link>
+          <div key={letter.id} className="w-full md:w-fit">
+            <Button variant="outline" className="w-full" asChild>
+              <Link
+                href={letter.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2"
+              >
+                {letter.company}
+                <ExternalLink className="size-4" />
+              </Link>
+            </Button>
+          </div>
         ))}
       </div>
     </section>
